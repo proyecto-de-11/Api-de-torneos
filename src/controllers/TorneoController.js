01218f1abd17fc4cc764b9d874a01cc7b70a3d74
Alan Campos <cs25001@esfe.agape.edu.sv>
2025-11-26 09:06:11 -0600

    Add getTorneoCreadoById function to TorneoController

--- src/controllers/TorneoController.js
+++ src/controllers/TorneoController.js
@@ -196,10 +196,37 @@ const getTorneoById = async (req, res) => {
     }
 };
 
+const getTorneoCreadoById = async (req, res) => {
+    const torneoId = req.params.id;
+
+   
+    if (!torneoId || isNaN(torneoId)) {
+        return res.status(400).json({ message: 'ID de torneo no válido o faltante' });
+    }
+
+    try {
+        const torneo = await torneoModel.getTorneoCreadoById(torneoId);
+
+        if (!torneo) {
+            return res.status(404).json({ message: `Torneo con id de creador  ${torneoId} no encontrado.` });
+        }
+
+        res.status(200).json(torneo);
+
+    } catch (error) {
+        console.error("❌ ERROR al obtener torneo por creador id  ❌:", error);
+        res.status(500).json({
+            message: 'Error interno del servidor al obtener el torneo creado por',
+            error: error.message,
+        });
+    }
+};
+
 export {
     createTorneo,
     updateTorneo,
     deleteTorneo,
     getAllTorneos,
-    getTorneoById
+    getTorneoById,
+    getTorneoCreadoById
 };
